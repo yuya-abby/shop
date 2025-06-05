6687082bbf0161ec89b507da80dd01868503a8fc
yuya-abby <huangtingyu97@gmail.com>
2025-06-05 14:31:11 +0800

    Delete 程式報告 directory

--- 程式報告/index-new.php
+++
@@ -1,9 +0,0 @@
-<?php
-include ("db.php");
-$img="";
-$money=$_POST["money"];
-
-$sql="INSERT INTO `addproduct`(`id`, `img`, `money`) VALUES (null,'$img','$money')";
-$res=mysqli_query($link,$sql);
-
-?>
